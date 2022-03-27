8e574061423dc4d938b630651c9c43520d031376
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-03-27 19:23:00 +0530

    Base commit: Level-03

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import InputContainer from "../InputContainer";
 
-const formFields = [
+interface formData {
+  id: number;
+  title: string;
+  formFields: formField[];
+}
+
+interface formField {
+  id: number;
+  label: string;
+  type: string;
+  value: string;
+}
+
+const intialFormFields: formField[] = [
   { id: 1, label: "First Name", type: "text", value: "" },
   { id: 2, label: "Last Name", type: "text", value: "" },
   { id: 3, label: "Email", type: "email", value: "" },
@@ -9,48 +22,122 @@ const formFields = [
   { id: 5, label: "Phone Number", type: "tel", value: "" },
 ];
 
+const getLocalForms: () => formData[] = () => {
+  const savedFormsJSON = localStorage.getItem("savedForms");
+  return savedFormsJSON ? JSON.parse(savedFormsJSON) : [];
+};
+
+const initialState: () => formData = () => {
+  const localForms = getLocalForms();
+
+  if (localForms.length > 0) {
+    return localForms[0];
+  }
+
+  const newForm = {
+    id: Number(new Date()),
+    title: "Untitled Form",
+    formFields: intialFormFields,
+  };
+
+  saveLocalForms([...localForms, newForm]);
+  return newForm;
+};
+
+const saveLocalForms = (localForms: formData[]) => {
+  localStorage.setItem("savedForms", JSON.stringify(localForms));
+};
+
+const saveFormData = (currentState: formData) => {
+  const localForms = getLocalForms();
+  const updatedLocalForms = localForms.map((form) =>
+    form.id === currentState.id ? currentState : form
+  );
+  saveLocalForms(updatedLocalForms);
+};
+
 export function Form(props: { closeFormCB: () => void }) {
-  const [state, setState] = useState(formFields);
+  const [state, setState] = useState(() => initialState());
   const [newField, setNewField] = useState("");
+  const titleRef = useRef<HTMLInputElement>(null);
+
+  // No dependencies => Mount and Unmount of component
+  useEffect(() => {
+    console.log("Component Mounted!");
+    const oldTitle = document.title;
+    document.title = "Form Editor";
+    titleRef.current?.focus();
+    return () => {
+      document.title = "React App";
+    };
+  }, []);
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      saveFormData(state);
+      console.log("state saved to localStorage");
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [state]);
 
   const addField = () => {
-    setState([
+    setState({
       ...state,
-      {
-        id: Number(new Date()),
-        label: newField,
-        type: "text",
-        value: "",
-      },
-    ]);
+      formFields: [
+        ...state.formFields,
+        {
+          id: Number(new Date()),
+          label: newField,
+          type: "text",
+          value: "",
+        },
+      ],
+    });
     setNewField("");
   };
 
   const removeField = (id: number) => {
-    setState(state.filter((field) => field.id !== id));
+    setState({
+      ...state,
+      formFields: state.formFields.filter((field) => field.id !== id),
+    });
   };
 
   const updateValue = (id: number, value: string) => {
-    setState(
-      state.map((field) => {
+    setState({
+      ...state,
+      formFields: state.formFields.map((field) => {
         if (field.id === id) return { ...field, value: value };
         return field;
-      })
-    );
+      }),
+    });
   };
 
   const clearAll = () => {
-    setState(
-      state.map((field) => {
+    setState({
+      ...state,
+      formFields: state.formFields.map((field) => {
         return { ...field, value: "" };
-      })
-    );
+      }),
+    });
   };
 
   return (
     <div className="flex flex-col gap-2 divide-y-2 divide-dotted p-4">
+      <input
+        type="text"
+        value={state.title}
+        className="my-2 w-full flex-1 rounded-lg border-2 border-gray-200 p-2"
+        onChange={(e) => {
+          setState({ ...state, title: e.target.value });
+        }}
+        ref={titleRef}
+      />
       <div>
-        {state.map((field) => (
+        {state.formFields.map((field) => (
           <InputContainer
             key={field.id}
             id={field.id}
@@ -81,8 +168,8 @@ export function Form(props: { closeFormCB: () => void }) {
 
       <div className="flex gap-4">
         <button
+          onClick={(_) => saveFormData(state)}
           className="my-2 w-1/4 rounded-xl bg-purple-500 p-2 text-white hover:bg-purple-700"
-          type="submit"
         >
           Submit
         </button>
